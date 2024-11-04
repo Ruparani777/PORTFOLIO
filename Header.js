93d5d360191ada6f65989d362d14d961627b26e3
Ruparani777 <96835039+Ruparani777@users.noreply.github.com>
2024-11-04 12:39:05 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Ruparani Thupakula</h1>
+    <p>Phone: +91 (939) 059-8723 | Email: roopathupakula01@gmail.com</p>
+    <p><a href="https://linkedin.com">LinkedIn</a> | <a href="https://github.com/Ruparani777">GitHub</a></p>
+  </header>
+);
+
+export default Header;
