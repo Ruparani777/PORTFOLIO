a6290bdde6673f9f1008cae6f0ce7e9b9c7e0529
Ruparani777 <96835039+Ruparani777@users.noreply.github.com>
2024-11-04 12:41:54 +0530

    Create Book.js

---
+++ Book.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Books = () => (
+  <section>
+    <h2>Books by Ruparani Thupakula</h2>
+    <ul>
+      <li><strong>Echoes of Her Intact Heart</strong> - A touching reflection on mother-daughter relationships.</li>
+      <li><strong>[The Epitome of slow living ]</strong> - The Epitome of Slow Living" follows Ishitha, a 23-year-old navigating anxiety and family pressures in a bustling city. Through her journey of self-discovery, she embraces slow living, creativity, and vulnerability, finding peace and connection in the beauty of everyday moments.</li>
+    </ul>
+  </section>
+);
+
+export default Books;
